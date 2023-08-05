28c87759d0dc3c212cb6514ae7c47324b61db153
Serter Iyigunlu <serteri@hotmail.com>
2023-08-06 04:01:32 +1000

    newone

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,7 @@
 
 import React, {useState , useEffect} from 'react';
 import {Routes,Route, useNavigate} from "react-router-dom";
-import { useDebounce } from 'use-debounce';
+
 import axios from 'axios';
 import './Register.css'
 export function RegisterPage(){
